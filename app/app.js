6c95f3856288c42d4eae55e9117b2c62d22f268e
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-10-18 14:53:35 -0500

    Add error handling

--- app/app.js
+++ app/app.js
@@ -1,12 +1,35 @@
+/*
+// Require packages
+*/
+
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+
+/*
+// Declare App
+*/
+
+
 const app = express();
+
+
+/*
+// Set up Packages
+*/
+
 
 app.use(bodyParser.urlencoded( { extended: false } ));
 app.use(cookieParser());
 app.set('view engine', 'pug');
+
+
+/*
+// Routes
+*/
+
 
 app.get('/', (req, res) => {
     const name = req.cookies.username;
@@ -38,6 +61,24 @@ app.post('/goodbye', (req, res) => {
 
 app.get('/cards', (req, res) => {
     res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is" });
+});
+
+
+/*
+// Other Middleware
+*/
+
+
+app.use((req, res, next) => {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status = err.status;
+    res.render('error');
 });
 
 app.listen(3000, () => {
