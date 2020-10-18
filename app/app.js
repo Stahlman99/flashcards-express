dda86f95376daf57f40efcc85fd684834d86db33
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-10-18 15:20:13 -0500

    Add route files and a placeholder data source

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
 /*
-// Require packages
+// Require resources
 */
 
 
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
 
 
 /*
@@ -25,43 +28,8 @@ app.use(bodyParser.urlencoded( { extended: false } ));
 app.use(cookieParser());
 app.set('view engine', 'pug');
 
-
-/*
-// Routes
-*/
-
-
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.render('index', { name });
-    } else {
-        res.redirect('/hello');
-    }
-});
-
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name) {
-        res.redirect('/');
-    } else {
-        res.render('hello');
-    }
-});
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
-app.get('/cards', (req, res) => {
-    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is" });
-});
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 
 /*
