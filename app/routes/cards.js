3585a18385638eeaea287329c944816f77cfb3ea
Nathan Stahlman <nathanstahlman99@gmail.com>
2020-10-18 19:09:55 -0500

    Add randomized cards and rerouting to correct card pages

--- app/routes/cards.js
+++ app/routes/cards.js
@@ -3,14 +3,24 @@ const router = express.Router();
 const { data } = require('../data/flashcardData.json');
 const { cards } = data;
 
+router.get('/', (req, res) => {
+    const num = Math.floor((Math.random() * cards.length))
+
+    res.redirect(`/cards/${num}?side=question`);
+});
+
 router.get('/:id', (req, res) => {
     const { side } = req.query;
     const { id } = req.params;
     const text = cards[id][side];
     const { hint } = cards[id];
+    const name = req.cookies.username;
 
+    if (!side) {
+        return res.redirect(`/cards/${id}?side=question`);
+    }
 
-    const templateData = { text, id };
+    const templateData = { text, id, name };
 
     if (side === 'question') {
         templateData.hint = hint;
